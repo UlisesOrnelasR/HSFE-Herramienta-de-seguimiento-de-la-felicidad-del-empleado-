04309ab3dc9f913dcfaddfd563ffd7cdce836788
UlisesFOrnelas <ornelas.ulises19@gmail.com>
2023-01-23 21:30:34 -0700

    Chore: Create people json

---
+++ src/data/people.ts
@@ -0,0 +1,137 @@
+export const People = [
+  {
+    "id": "1",
+    "name": "Sarah Williams",
+    "category": "Technology",
+    "company": "Apple",
+    "levelOfHappiness": 8,
+    "category-image": "https://www.example.com/technology.jpg",
+    "company-image": "https://www.example.com/apple.jpg"
+  },
+  {
+    "id": "2",
+    "name": "Michael Brown",
+    "category": "Finance",
+    "company": "JP Morgan",
+    "levelOfHappiness": 7,
+    "category-image": "https://www.example.com/finance.jpg",
+    "company-image": "https://www.example.com/jpmorgan.jpg"
+  },
+  {
+    "id": "3",
+    "name": "Emily Davis",
+    "category": "Healthcare",
+    "company": "Johnson & Johnson",
+    "levelOfHappiness": 9,
+    "category-image": "https://www.example.com/healthcare.jpg",
+    "company-image": "https://www.example.com/johnsonandjohnson.jpg"
+  },
+  {
+    "id": "4",
+    "name": "Matthew Miller",
+    "category": "Retail",
+    "company": "Walmart",
+    "levelOfHappiness": 6,
+    "category-image": "https://www.example.com/retail.jpg",
+    "company-image": "https://www.example.com/walmart.jpg"
+  },
+  {
+    "id": "5",
+    "name": "Nicholas Garcia",
+    "category": "Energy",
+    "company": "ExxonMobil",
+    "levelOfHappiness": 7,
+    "category-image": "https://www.example.com/energy.jpg",
+    "company-image": "https://www.example.com/exxonmobil.jpg"
+  },
+  {
+    "id": "6",
+    "name": "Amanda Thompson",
+    "category": "Consumer Goods",
+    "company": "Procter & Gamble",
+    "levelOfHappiness": 8,
+    "category-image": "https://www.example.com/consumer-goods.jpg",
+    "company-image": "https://www.example.com/procterandgamble.jpg"
+  },
+  {
+    "id": "7",
+    "name": "Daniel Gonzalez",
+    "category": "Construction",
+    "company": "Bechtel",
+    "levelOfHappiness": 6,
+    "category-image": "https://www.example.com/construction.jpg",
+    "company-image": "https://www.example.com/bechtel.jpg"
+  },
+  {
+    "id": "8",
+    "name": "Andrew Wilson",
+    "category": "Transportation",
+    "company": "UPS",
+    "levelOfHappiness": 7,
+    "category-image": "https://www.example.com/transportation.jpg",
+    "company-image": "https://www.example.com/ups.jpg"
+  },
+  {
+    "id": "9",
+    "name": "Alexandra Martinez",
+    "category": "Automotive",
+    "company": "Ford",
+    "levelOfHappiness": 8,
+    "category-image": "https://www.example.com/automotive.jpg",
+    "company-image": "https://www.example.com/ford.jpg"
+  },
+  {
+    "id": "10",
+    "name": "Jonathan Anderson",
+    "category": "Aerospace",
+    "company": "Boeing",
+    "levelOfHappiness": 7,
+    "category-image": "https://www.example.com/aerospace.jpg",
+    "company-image": "https://www.example.com/boeing.jpg"
+  },
+  {
+    "id": "11",
+    "name": "Kevin Martinez",
+    "category": "Telecommunications",
+    "company": "AT&T",
+    "levelOfHappiness": 6,
+    "category-image": "https://www.example.com/telecommunications.jpg",
+    "company-image": "https://www.example.com/att.jpg"
+  },
+  {
+    "id": "12",
+    "name": "Cynthia Taylor",
+    "category": "Pharmaceuticals",
+    "company": "Pfizer",
+    "levelOfHappiness": 9,
+    "category-image": "https://www.example.com/pharmaceuticals.jpg",
+    "company-image": "https://www.example.com/pfizer.jpg"
+  },
+  {
+    "id": "13",
+    "name": "Adam Smith",
+    "category": "Environmental Services",
+    "company": "Waste Management",
+    "levelOfHappiness": 8,
+    "category-image": "https://www.example.com/environmental-services.jpg",
+    "company-image": "https://www.example.com/waste-management.jpg"
+  },
+  {
+    "id": "14",
+    "name": "Karen Clark",
+    "category": "Mining",
+    "company": "BHP",
+    "levelOfHappiness": 6,
+    "category-image": "https://www.example.com/mining.jpg",
+    "company-image": "https://www.example.com/bhp.jpg"
+  },
+  {
+    "id": "15",
+    "name": "Christine Garcia",
+    "category": "Food & Beverage",
+    "company": "Coca-Cola",
+    "levelOfHappiness": 7,
+    "category-image": "https://www.example.com/food-beverage.jpg",
+    "company-image": "https://www.example.com/coca-cola.jpg"
+  }
+]
