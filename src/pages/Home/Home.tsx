27a7d9cf05c8b844efe82525f133bd7b4fd380b2
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-02-16 14:54:11 -0700

    chore: Create models

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data";
+import { Person } from "@/models/people";
 
 export interface HomeInterface {}
 
 export const Home: React.FC<HomeInterface> = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
   const pageSizes = 5;
+
   const columns = [
+    {
+      field: "actions",
+      headerName: "",
+      width: 50,
+      renderCell: (params: GridRenderCellParams) => <>{}</>,
+    },
     {
       field: "name",
       headerName: "Name",
       flex: 1,
       minWidth: 150,
-      renderCell: (params: GridRenderCellParams) => <>{params.row.name}</>,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
     },
   ];
 
   return (
-    <div>
+    <>
       <DataGrid
         disableColumnSelector
         disableSelectionOnClick
@@ -26,7 +49,8 @@ export const Home: React.FC<HomeInterface> = () => {
         rowsPerPageOptions={[pageSizes]}
         rows={People}
         columns={columns}
+        getRowId={(row) => row.id}
       />
-    </div>
+    </>
   );
 };
