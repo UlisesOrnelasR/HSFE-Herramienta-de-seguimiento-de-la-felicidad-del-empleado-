b7c40fedf6c754eb751722ae230f9931cf7e4e85
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-02-16 17:02:03 -0700

    refactor: People table

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,86 +1,25 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data";
 import { Person } from "@/models/people";
 import { Checkbox } from "@mui/material";
-import { addFavorite } from "@/redux/states";
+import { addFavorite, addPeople } from "@/redux/states";
+import { AppStore } from "@/redux/store";
+import { PeopleTable } from "./components/PeopleTable";
 
 export interface HomeInterface {}
 
 export const Home: React.FC<HomeInterface> = () => {
-  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
-  const pageSizes = 5;
   const dispatch = useDispatch();
 
-  const findPerson = (person: Person) =>
-    !!selectedPeople.find((p) => p.id === person.id);
-
-  const filterPerson = (person: Person) =>
-    selectedPeople.filter((p) => p.id !== person.id);
-
-  const handleChange = (person: Person) => {
-    const filteredPeople = findPerson(person)
-      ? filterPerson(person)
-      : [...selectedPeople, person];
-    dispatch(addFavorite(filteredPeople));
-    setSelectedPeople(filteredPeople);
-  };
-
-  const columns = [
-    {
-      field: "actions",
-      type: "actions",
-      sortable: false,
-      headerName: "",
-      width: 50,
-      renderCell: (params: GridRenderCellParams) => (
-        <>
-          {
-            <Checkbox
-              size="small"
-              checked={findPerson(params.row)}
-              onChange={() => handleChange(params.row)}
-            />
-          }
-        </>
-      ),
-    },
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      minWidth: 150,
-      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-    },
-    {
-      field: "category",
-      headerName: "Categories",
-      flex: 1,
-      minWidth: 150,
-      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-    },
-    {
-      field: "company",
-      headerName: "Company",
-      flex: 1,
-      minWidth: 150,
-      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-    },
-  ];
+  useEffect(() => {
+    dispatch(addPeople(People));
+  }, []);
 
   return (
     <>
-      <DataGrid
-        disableColumnSelector
-        disableSelectionOnClick
-        autoHeight
-        pageSize={pageSizes}
-        rowsPerPageOptions={[pageSizes]}
-        rows={People}
-        columns={columns}
-        getRowId={(row) => row.id}
-      />
+      <PeopleTable />
     </>
   );
 };
